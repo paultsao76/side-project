3a4f550da7825bf191efdcb3676b6dbaed4bfd93
paultsao76 <paultsao76@gmail.com>
2025-01-23 16:03:00 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <div>
  		Copyright &copy;PET BOOK All Rights Reserved.<br>
 		PET BOOK is the registered trademark of Copyright<br>
-		E-mail：paultsao76@gmai.com<br>
+		E-mail：paultsao76@gmail.com<br>
 		tel：0981981396
 		
 	</div>
